498c5442f5249f84d5463cd16b45e18bcf595f94
Kai Burjack <kburjack@googlemail.com>
2015-11-25 15:01:03 +0100

    Add demo for GL context sharing
    This demo shows two canvases in the same window.

---
+++ test/org/lwjgl/opengl/swt/SharedContextsDemo.java
@@ -0,0 +1,167 @@
+package org.lwjgl.opengl.swt;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+
+/**
+ * Showcases context sharing.
+ * 
+ * @author Kai Burjack
+ */
+public class SharedContextsDemo {
+    public static void main(String[] args) {
+        int minClientWidth = 600;
+        int minClientHeight = 300;
+        final Display display = new Display();
+        final Shell shell = new Shell(display, SWT.SHELL_TRIM);
+        GridLayout layout = new GridLayout(2, false);
+        shell.setLayout(layout);
+        int dw = shell.getSize().x - shell.getClientArea().width;
+        int dh = shell.getSize().y - shell.getClientArea().height;
+        shell.setMinimumSize(minClientWidth + dw, minClientHeight + dh);
+        GLData data = new GLData();
+        data.swapInterval = 1;
+        data.samples = 2;
+        data.doubleBuffer = true;
+        final GLCanvas canvas0 = new GLCanvas(shell, SWT.NO_BACKGROUND | SWT.NO_REDRAW_RESIZE, data);
+        canvas0.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        data.shareContext = canvas0;
+        final GLCanvas canvas1 = new GLCanvas(shell, SWT.NO_BACKGROUND | SWT.NO_REDRAW_RESIZE, data);
+        canvas1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        final GLCanvas[] canvases = { canvas0, canvas1 };
+
+        // Create GLCapabilities in the first context
+        canvas0.setCurrent();
+        GL.createCapabilities();
+
+        shell.addListener(SWT.Traverse, new Listener() {
+            public void handleEvent(Event event) {
+                switch (event.detail) {
+                case SWT.TRAVERSE_ESCAPE:
+                    shell.close();
+                    event.detail = SWT.TRAVERSE_NONE;
+                    event.doit = false;
+                    break;
+                }
+            }
+        });
+
+        // Create resources in the first context
+
+        // Create a simple shader program
+        final int program = glCreateProgram();
+        int vs = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vs,
+                "uniform float rot;" +
+                "uniform float aspect;" +
+                "void main(void) {" + 
+                "  vec4 v = gl_Vertex * 0.5;" +
+                "  vec4 v_ = vec4(0.0, 0.0, 0.0, 1.0);" +
+                "  v_.x = v.x * cos(rot) - v.y * sin(rot);" +
+                "  v_.y = v.y * cos(rot) + v.x * sin(rot);" +
+                "  v_.x /= aspect;" +
+                "  gl_Position = v_;" +
+                "}");
+        glCompileShader(vs);
+        glAttachShader(program, vs);
+        int fs = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fs,
+                "void main(void) {" +
+                "  gl_FragColor = vec4(0.1, 0.3, 0.5, 1.0);" + 
+                "}");
+        glCompileShader(fs);
+        glAttachShader(program, fs);
+        glLinkProgram(program);
+        glUseProgram(program);
+        final int rotLocation = glGetUniformLocation(program, "rot");
+        final int aspectLocation = glGetUniformLocation(program, "aspect");
+
+        // Create a simple quad
+        final int vbo = glGenBuffers();
+        final int ibo = glGenBuffers();
+        float[] vertices = { 
+            -1, -1, 0,
+             1, -1, 0,
+             1,  1, 0,
+            -1,  1, 0
+        };
+        int[] indices = {
+            0, 1, 2,
+            2, 3, 0
+        };
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, (FloatBuffer) BufferUtils.createFloatBuffer(vertices.length).put(vertices).flip(), GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, (IntBuffer) BufferUtils.createIntBuffer(indices.length).put(indices).flip(), GL_STATIC_DRAW);
+
+        // Bind objects in each context and set state
+        for (GLCanvas canvas : canvases) {
+            canvas.setCurrent();
+            glClearColor(0.4f, 0.6f, 0.9f, 1.0f);
+            final int vao = glGenVertexArrays();
+            glBindVertexArray(vao);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0L);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+            glEnableVertexAttribArray(0);
+            glUseProgram(program);
+        }
+
+        shell.setSize(600, 300);
+        shell.open();
+
+        display.asyncExec(new Runnable() {
+            float rot;
+            long lastTime = System.nanoTime();
+
+            public void run() {
+                // Render in each context
+                for (int i = 0; i < canvases.length; i++) {
+                    GLCanvas canvas = canvases[i];
+                    if (canvas.isDisposed()) {
+                        return;
+                    }
+                    canvas.setCurrent();
+                    glClear(GL_COLOR_BUFFER_BIT);
+                    glViewport(0, 0, canvas.getSize().x, canvas.getSize().y);
+                    float aspect = (float) canvas.getSize().x / canvas.getSize().y;
+                    glUniform1f(aspectLocation, aspect);
+                    glUniform1f(rotLocation, rot);
+                    glDrawElements(GL11.GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+                    canvas.swapBuffers();
+                }
+                display.asyncExec(this);
+
+                long thisTime = System.nanoTime();
+                float delta = (thisTime - lastTime) / 1E9f;
+                rot += delta * 0.1f;
+                if (rot > 2.0 * Math.PI) {
+                    rot -= 2.0f * (float) Math.PI;
+                }
+                lastTime = thisTime;
+            }
+        });
+
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch())
+                display.sleep();
+        }
+        display.dispose();
+    }
+}
